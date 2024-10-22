cfe89377a051b2c0c168a37cf76c2fede6a7c441
Dekiuv <diego1roque2@gmail.com>
2024-10-22 14:12:58 +0200

    Estaciones nuevas L2, L4

--- Main.py
+++ Main.py
@@ -19,6 +19,9 @@ destino_ = True
 
 while menu == True:
     while inicio_ == True:
+        # Mostrar solo los nombres de las estaciones en formato de lista
+        for estacion in es.estaciones:
+            print(estacion)
         inicio =  input("Escriba punto de inicio...")
         if inicio in es.estaciones:
             print ("Estación encontrada")
@@ -47,28 +50,28 @@ plt.figure(figsize=(8, 6))  # Ajustar el tamaño de la figura
 # Dibujar las líneas (aristas)
 for edge in co.aristas:
     # L12
-    if edge == ('Sarria', 'Reina Elisenda'):
+    if edge == ('Sarria', 'Reina elisenda'):
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L12, alpha=0.8, linewidth=6)
     # L11
     elif edge in [('Trinitat nova', "Casa de l'aigua"), ("Casa de l'aigua", 'Torre baró vallbona'), ('Torre baró vallbona', 'Ciutat meridiana'), ('Ciutat meridiana', 'Can cuias')]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L11, alpha=0.8, linewidth=6)
     # L7
-    elif edge in [('Gracia', 'Plaça Molina'), ('Plaça Molina', 'Padua'), ('Padua', 'El Putxet'), ('El Putxet', 'Tibidabo')]:
+    elif edge in [('Gracia', 'Plaça molina'), ('Plaça molina', 'Padua'), ('Padua', 'El putxet'), ('El putxet', 'Tibidabo')]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L7, alpha=0.8, linewidth=6)
     # L6
-    elif edge in [('Catalunya', 'Provenca'), ('Provenca', 'Gracia'), ('Gracia', 'Sant Gervasi'), ('Sant Gervasi', 'Muntaner'), ('Muntaner', 'La Bonanova'), ('La Bonanova', 'Les tres torres'), ('Les tres torres', 'Sarria')]:
+    elif edge in [('Catalunya', 'Provenca'), ('Provenca', 'Gracia'), ('Gracia', 'Sant gervasi'), ('Sant gervasi', 'Muntaner'), ('Muntaner', 'La bonanova'), ('La bonanova', 'Les tres torres'), ('Les tres torres', 'Sarria')]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L6, alpha=0.8, linewidth=6)
     # L5
-    elif edge in [('Sants', 'Entença'), ('Entença', 'Hospital clinic'), ('Hospital clinic', 'Diagonal'),]:
+    elif edge in [('Sants', 'Entença'), ('Entença', 'Hospital clinic'), ('Hospital clinic', 'Diagonal'), ('Diagonal', 'Verdaguer'), ('Verdaguer', 'Sagrada familia'),]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L5, alpha=0.8, linewidth=6)
     # L4
-    elif edge in [('Passeig de gracia', 'Urquinaona'),]:
+    elif edge in [('Verdaguer', 'Girona'), ('Girona', 'Passeig de gracia'), ('Passeig de gracia', 'Urquinaona'),]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L4, alpha=0.8, linewidth=6)
     # L3
     elif edge in [('Trinitat nova', 'Roquetes'), ('Roquetes', 'Canyelles'), ('Canyelles', 'Valldaura'),('Valldaura', 'Mundet'),('Mundet', 'Montbau'),('Montbau', "Vall d'hebron"),("Vall d'hebron", "Penitents"),('Penitents', 'Vallcarca'),('Vallcarca', 'Lesseps'),('Lesseps', 'Fontana'),('Fontana', 'Diagonal'),('Diagonal', 'Passeig de gracia'),('Passeig de gracia', 'Catalunya'),('Catalunya', 'Liceu'), ('Liceu', 'Drassanes'), ('Drassanes', 'Paral·lel'), ('Paral·lel', 'Poble sec'), ('Poble sec', 'Espanya'), ('Espanya', 'Tarragona'), ('Tarragona', 'Sants'), ('Sants', 'Plaça del centre'), ('Plaça del centre', 'Les corts'), ('Les corts', 'Maria cristina'), ('Maria cristina', 'Palau reial'), ('Palau reial', 'Zona universitaria')]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L3, alpha=0.8, linewidth=6)
     # L2
-    elif edge in [('Paral·lel', 'Sant antoni'), ('Sant antoni', 'Universitat'), ('Universitat', 'Passeig de gracia'), ('Passeig de gracia', 'Tetuan')]:
+    elif edge in [('Paral·lel', 'Sant antoni'), ('Sant antoni', 'Universitat'), ('Universitat', 'Passeig de gracia'), ('Passeig de gracia', 'Tetuan'), ('Tetuan', 'Monumental'), ('Monumental', 'Sagrada familia'),]:
         plt.plot(*zip(es.estaciones[edge[0]], es.estaciones[edge[1]]), color=col.line_color_L2, alpha=0.8, linewidth=6)
     # L1
     else:
